9706163d4c6c0ef6f5e4e71bcc61db5f648e3918
alezanper <fabenavideso@gmail.com>
2018-09-08 21:40:29 -0500

    Using Console

---
+++ UsingConsole/consoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/**
+ * Using Console application
+ * */
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             for(int i = 0; i<=100; i++)
+            {
+                // Printing only even numbers
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
