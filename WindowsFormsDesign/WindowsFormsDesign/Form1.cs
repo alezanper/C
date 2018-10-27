a5e8dc788c716412b137ac95c15543c3f4c2d8f6
alezanper <fabenavideso@gmail.com>
2018-10-27 17:32:36 -0500

    using forms

---
+++ WindowsFormsDesign/WindowsFormsDesign/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsDesign {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e) {
+            label1.Text = dateTimePicker1.Value.ToLongDateString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "button was pressed!";
+        }
+    }
+}
