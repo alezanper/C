a7ff34df76ac6539551f7eb3b3018d5e684ee730
alezanper <fabenavideso@gmail.com>
2019-05-12 01:33:39 -0500

    console

---
+++ ConsoleBasics/ConsoleBasics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+            double maxd = double.MaxValue;
+            double mind = double.MinValue;
+            Console.WriteLine($"The range of double is {mind} to {maxd}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+
+            decimal mindc = decimal.MinValue;
+            decimal maxdc = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {mindc} to {maxdc}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+        }
+    }
+}
