09810eb268b57976f8830f7e70df63166da0d4f8
Alexander Benavides <fabenavideso@gmail.com>
2020-04-08 21:12:13 -0500

    string functions

--- ConsoleBasics/ConsoleBasics/Program.cs
+++ ConsoleBasics/ConsoleBasics/Program.cs
@@ -6,52 +6,52 @@ namespace ConsoleBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("The current time is " + DateTime.Now);
+            Console.WriteLine("The current time is: " + DateTime.Now + "\n");
+
+            Console.WriteLine("Using int:");
             int max = int.MaxValue;
             int min = int.MinValue;
-            Console.WriteLine($"The range of integers is {min} to {max}");
+            Console.WriteLine($"The range of integers is {min} to {max}\n");
 
+            Console.WriteLine("Using double:");
             double maxd = double.MaxValue;
             double mind = double.MinValue;
-            Console.WriteLine($"The range of double is {mind} to {maxd}");
+            Console.WriteLine($"The range of double is {mind} to {maxd}\n");
 
+            Console.WriteLine("1.0/3.0 usind double is:");
             double third = 1.0 / 3.0;
-            Console.WriteLine(third);
+            Console.WriteLine(third + "\n");
 
             decimal mindc = decimal.MinValue;
             decimal maxdc = decimal.MaxValue;
             Console.WriteLine($"The range of the decimal type is {mindc} to {maxdc}");
 
-            double a = 1.0;
-            double b = 3.0;
-            Console.WriteLine(a / b);
-
+            Console.WriteLine("1.0M/3.0M usind decimal is:");
             decimal c = 1.0M;
             decimal d = 3.0M;
-            Console.WriteLine(c / d);
+            Console.WriteLine(c / d + "\n");
 
+            Console.WriteLine("String functions:");
             string p = "Alexander";
             Console.WriteLine($"My name is {p}");
             Console.WriteLine($"{p} has {p.Length} letters");
 
+            Console.WriteLine("Trim:");
             string q = "     Hello World!     ";
-            Console.WriteLine($"Without Trim:");
             Console.WriteLine($"{q}");
-            Console.WriteLine($"Using Trim:");
             Console.WriteLine($"{q.Trim()}");
 
+            Console.WriteLine("Replace hello with Bye");
             string r = "Hello World!";
-            Console.WriteLine($"{r.Replace("Hello", "Bye")}");
+            Console.WriteLine($"{r.Replace("Hello", "Bye")} \n");
 
-            Console.WriteLine($"{r.ToUpper()}");
-            Console.WriteLine($"{r.ToLower()}");
+            Console.WriteLine($"Function to {r}");
+            Console.WriteLine($"Upper: {r.ToUpper()}");
+            Console.WriteLine($"Lower: {r.ToLower()} \n");
 
             string s = "jarvis is my copilot";
+            Console.WriteLine($"jarvis is in the phrase '{s}'?");
             Console.WriteLine(s.Contains("jarvis"));
-
-
-
         }
     }
 }
