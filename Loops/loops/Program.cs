e26c5e0ab27fca145f28b20fab0587f55d5ed226
Alexander Benavides <fabenavideso@gmail.com>
2020-04-08 21:40:56 -0500

    using loops

---
+++ Loops/loops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int limit = 5;
+            Console.WriteLine("Using While:");
+            while (counter < limit)
+            {
+                Console.WriteLine($"counter value is: {counter}");
+                counter++;
+            }
+
+            counter = 0;
+            Console.WriteLine("\nUsing do-While:");
+            do
+            {
+                Console.WriteLine($"counter value is: {counter}");
+                counter++;
+            } while (counter < limit);
+
+            counter = 0;
+            Console.WriteLine("\nUsing for:");
+
+            for(int i = 0; i < limit; i++)
+            {
+                Console.WriteLine($"counter value is: {counter}");
+                counter++;
+            }
+
+        }
+    }
+}
