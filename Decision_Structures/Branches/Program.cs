c65c3c2829e98e008fe429b11b973b8fcf5b0cba
Alexander Benavides <fabenavideso@gmail.com>
2020-04-08 21:24:24 -0500

    adding simple branches

---
+++ Decision_Structures/Branches/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Branches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 40;
+            int b = 50;
+            
+            if (a + b > 100)
+                Console.WriteLine("Answer is greater than 100");
+            else
+                Console.WriteLine("Answer is lower than 100");
+
+
+        }
+    }
+}
